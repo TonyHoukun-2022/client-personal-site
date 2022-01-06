cee3fde29e00fef320fdcc1a0949bdd4f24a66c6
Tony Peng <lp1dxing@gmail.com>
2022-01-06 19:55:18 +1100

    fix navBar bug

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
   //   let y = e.clientY
   //   $(".lm-animated-bg").css("background-position", "calc( 50% + " + x * -0.008 + "px ) calc( 50% + " + y * -0.008 + "px )")
   // })
+
   // Homepage Animation
   const toggleAnimationClass = () => {
     $(".subtitle1").toggleClass("animate__animated")
@@ -17,48 +18,149 @@ $(document).ready(() => {
   }
   toggleAnimationClass()
 
-  // Bootstrap Navbar tooltips js code
-  // [].slice.call(arguments)能将具有length属性的对象转成数组。
-  // var tooltipTriggerList = [].slice.call($('[data-bs-toggle="tooltip"]'))
-  // var tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => {
-  //   return new bootstrap.Tooltip(tooltipTriggerEl)
-  // })
-
   // display tooltip
   $('[data-bs-toggle="tooltip"]').tooltip()
-})
 
-//Section switch by navbar
-const animations = [
-  "animate__backInDown",
-  "animate__bounceInUp",
-  "animate__bounceInLeft",
-  "animate__flipInX",
-  "animate__rotateIn",
-  "animate__rotateInDownLeft",
-  "animate__rollIn",
-  "animate__zoomIn",
-  "animate__slideInLeft",
-]
-function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-$("nav ul li a").each(function (i) {
-  $(this).on("click", () => {
-    //first remove all the active class of nav <a>
-    $("nav ul li a").each(function () {
-      $(this).removeClass("active")
+  //form validation
+  $("#contactForm").validate({
+    errorClass: "error-message",
+    // validClass: "valid success-alert",
+    rules: {
+      name : {
+        required: true,
+        minlength: 2
+      },
+      age: {
+        require: {
+          depends: function () {
+            return $("#age").val() > 18 && $("#age").val() < 50
+          }
+        },
+        number: true,
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      subject: {
+        required: true,
+      },
+      message: {
+        required: true,
+        minlength:10
+      }
+    },
+    messages : {
+      name: {
+        minlength: "Name should be at least 2 characters"
+      },
+      age: {
+        required: "Please enter your age btw 18 and 50",
+        number: "Please enter your age as a numerical value",
+        // min: "You must be at least 18 years old"
+      },
+      email: {
+        email: "The email should be in the format: abc@domain.tld"
+      },
+      subject: {
+        required: "subject is required."
+      },
+      message: {
+        minlength : "message must be at least 10 characters"
+      }
+    },
+    submitHandler: function (form) {
+      form.submit()
+    }
+  });
+
+
+  //Section switch by navbar
+  const animations = [
+    "animate__backInDown",
+    "animate__bounceInUp",
+    "animate__bounceInLeft",
+    "animate__flipInX",
+    "animate__rotateIn",
+    "animate__rotateInDownLeft",
+    "animate__rollIn",
+    "animate__zoomIn",
+    "animate__slideInLeft",
+  ]
+  function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  //navbar section
+  $("nav ul li a").each(function (i, e) {
+    console.log(i, e)
+    $(this).on("click", () => {
+      console.log(i, e)
+      //first remove all the active class of nav <a>
+      $("nav ul li a").each(function () {
+        $(this).removeClass("active")
+      })
+      //add the active class to the clicked <a>
+      $(this).addClass("active")
+      $(".animated-section").each(function (index, ele) {
+        console.log(index, ele)
+        //remove section-active for all <section>
+        $(this).removeClass("section-active")
+        // if the index of the < a > matches the index of the < section >, means a tag matches the relevant page
+        if (i === index) {
+          console.log(index)
+          // get the class attribute of <section> and split into an array of strings of classNames
+          let classNames = $(this).attr("class").split(" ")
+          classNames.forEach((className) => {
+            if (animations.includes(className)) $(this).removeClass(className)
+          })
+          let random = getRandomInt(0, animations.length - 1)
+          $(this).addClass(`section-active ${animations[random]}`)
+        }
+      })
     })
-    //add the active class to the clicked <a>
-    $(this).addClass("active")
-    $("section").each(function (index) {
-      //remove section-active for all <section>
+  })
+
+  //section switch by arrows
+  $(".arrow-right").on("click", () => {
+    let nextPageIndex = null
+    $("nav ul li a").each(function (i) {
+      let className = $(this).attr("class")
+      if (className === "active") {
+        nextPageIndex = (i + 1) % 6
+        $(this).removeClass("active")
+      }
+    })
+    $("nav ul li a").eq(nextPageIndex).addClass("active")
+    $(".animated-section").each(function (index) {
       $(this).removeClass("section-active")
-      // if the index of the < a > matches the index of the < section >, means a tag matches the relevant page
-      if (i === index) {
-        // get the class attribute of <section> and split into an array of strings of classNames
+      if (nextPageIndex === index) {
+        let classNames = $(this).attr("class").split(" ")
+        classNames.forEach((className) => {
+          if (animations.includes(className)) $(this).removeClass(className)
+        })
+        let random = getRandomInt(0, animations.length - 1)
+        $(this).addClass(`section-active ${animations[random]}`)
+        // $(this).addClass("section-active")
+      }
+    })
+  })
+  $(".arrow-left").on("click", () => {
+    let previousPageIndex = null
+    $("nav ul li a").each(function (i) {
+      let className = $(this).attr("class")
+      if (className === "active") {
+        previousPageIndex = i - 1
+        previousPageIndex === -1 ? (previousPageIndex = 5) : previousPageIndex
+        $(this).removeClass("active")
+      }
+    })
+    $("nav ul li a").eq(previousPageIndex).addClass("active")
+    $("section").each(function (index) {
+      $(this).removeClass("section-active")
+      if (previousPageIndex === index) {
         let classNames = $(this).attr("class").split(" ")
         classNames.forEach((className) => {
           if (animations.includes(className)) $(this).removeClass(className)
@@ -68,95 +170,57 @@ $("nav ul li a").each(function (i) {
       }
     })
   })
-})
-//section switch by arrows
-$(".arrow-right").on("click", () => {
-  let currentIndex = null
-  $("nav ul li a").each(function (i) {
-    let className = $(this).attr("class")
-    if (className === "active") {
-      currentIndex = (i + 1) % 6
-      $(this).removeClass("active")
-    }
-  })
-  $("nav ul li a").eq(currentIndex).addClass("active")
-  $("section").each(function (index) {
-    $(this).removeClass("section-active")
-    if (currentIndex === index) {
-      let classNames = $(this).attr("class").split(/\s+/)
-      classNames.forEach((className) => {
-        if (animations.includes(className)) $(this).removeClass(className)
-      })
-      let random = getRandomInt(0, animations.length - 1)
-      $(this).addClass(`section-active ${animations[random]}`)
-    }
-  })
-})
-$(".arrow-left").on("click", () => {
-  let currentIndex = null
-  $("nav ul li a").each(function (i) {
-    let className = $(this).attr("class")
-    if (className === "active") {
-      currentIndex = i - 1
-      currentIndex === -1 ? (currentIndex = 5) : currentIndex
-      $(this).removeClass("active")
-    }
-  })
-  $("nav ul li a").eq(currentIndex).addClass("active")
-  $("section").each(function (index) {
-    $(this).removeClass("section-active")
-    if (currentIndex === index) {
-      let classNames = $(this).attr("class").split(/\s+/)
-      classNames.forEach((className) => {
-        if (animations.includes(className)) $(this).removeClass(className)
-      })
-      let random = getRandomInt(0, animations.length - 1)
-      $(this).addClass(`section-active ${animations[random]}`)
-    }
-  })
-})
-//Carousel about-me-testimonials
-$(".about-me-testimonials .owl-carousel").owlCarousel({
-  loop: false,
-  nav: true,
-  margin: 10,
-  responsive: {
-    0: {
-      items: 1,
+  //Carousel about-me-testimonials
+  $(".about-me-testimonials .owl-carousel").owlCarousel({
+    loop: false,
+    nav: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
     },
-    600: {
-      items: 2,
-    },
-  },
-})
+  })
 
-//carousel about-me-clients
-$(".about-me-clients .owl-carousel").owlCarousel({
-  loop: true,
-  nav: true,
-  margin: 10,
-  responsive: {
-    0: {
-      items: 1,
+  //carousel about-me-clients
+  $(".about-me-clients .owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 5,
+      },
     },
-    600: {
-      items: 5,
-    },
-  },
+  })
 })
 
 //Menu toggle
-$(window)
-  .resize(function () {
-    if ($(window).width() <= 992) {
-      $(".site-header").addClass("animate__slideOutRight")
-    } else {
-      $(".site-header").removeClass("animate__slideOutRight")
-    }
-  })
-  .resize()
+var siteHeader = $(".site-header")
+$(window).resize(function () {
+  var win = $(this)
+  if (win.width() <= 992) {
+    siteHeader.addClass("animate__slideOutRight")
+    siteHeader.css("display", "none")
+    //tooltip placement
+    $(".list-group>.list-group-item").attr("data-bs-placement", "top")
+  } else {
+    siteHeader.removeClass("animate__slideOutRight")
+    siteHeader.css("display", "")
+    $(".list-group>.list-group-item").attr("data-bs-placement", "left")
+  }
+}).resize()
+
 if ($(window).width() <= 992) {
+  siteHeader.addClass("animate__slideOutRight")
   $(".menu-toggle").on("click", () => {
+    siteHeader.css("display","block")
     $(".menu-toggle > i").toggleClass("lnr-menu lnr-cross")
     $(".site-header").toggleClass("animate__slideInRight animate__slideOutRight")
   })
